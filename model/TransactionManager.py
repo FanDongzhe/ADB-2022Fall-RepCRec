255eb613e0eb266ba4a9ce85a19968ad3abeb80b
FanDongzhe <ff294586320@163.com>
2022-11-28 02:35:44 -0500

    add basic functions

---
+++ model/TransactionManager.py
@@ -0,0 +1,35 @@
+from Transaction import Transaction
+
+class TransactionManager():
+    def __init__(self, transaction_list, instruction_queue, current_time) -> None:
+
+        self.transaction_list = transaction_list
+        self.instruction_queue = instruction_queue
+        self.current_time = current_time
+    
+    def simulation():
+        pass
+
+    def execute():
+        pass
+
+    def get_sites():
+        pass
+
+    def read(variableid, transactionid):
+        pass
+
+    def write(variableid, transactionid):
+        pass
+
+    def dump():
+        pass
+
+    def fail(transactionid):
+        pass 
+
+    def create_transaction():
+        pass
+
+    def delete_transaction():
+        pass
